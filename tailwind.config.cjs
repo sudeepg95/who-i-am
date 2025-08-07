5b71f5d7c22a570af0a20bac236bd9c73286e4f1
Sudeep G <sudeep.giri@travelopia.com>
2025-08-07 21:09:00 +0530

    Bug fixes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+  darkMode: "[data-theme='dark']",
   theme: {
     extend: {
       fontFamily: {
@@ -10,6 +11,19 @@ module.exports = {
       colors: {
         primary: "var(--color-primary)",
         secondary: "var(--color-secondary)",
+        backdrop: "var(--color-backdrop)",
+        shadow: "var(--color-shadow)",
+        "shadow-light": "var(--color-shadow-light)",
+        "text-shadow": "var(--color-text-shadow)",
+        white: "var(--color-white)",
+        "blue-300": "var(--color-blue-300)",
+        "gray-300": "var(--color-gray-300)",
+        "gray-400": "var(--color-gray-400)",
+        "blue-500": "var(--color-blue-500)",
+        "purple-500": "var(--color-purple-500)",
+        "gradient-blue": "var(--color-gradient-blue)",
+        "gradient-purple": "var(--color-gradient-purple)",
+        "gradient-pink": "var(--color-gradient-pink)",
       },
       textColor: {
         default: "var(--color-text)",
