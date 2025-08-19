b1a4b6169e2e735cee550ea9c8780c41d335649e
Sudeep G <sudeep.giri@travelopia.com>
2025-08-19 12:43:15 +0530

    Update resume link

--- src/data/user-profile.ts
+++ src/data/user-profile.ts
@@ -1,157 +1,158 @@
 const userProfile = {
-  "personal": {
-    "name": "Sudeep G",
-    "title": "Lead Software Engineer",
-    "location": {
-      "city": "Bengaluru",
-      "state": "Karnataka",
-      "country": "India",
-      "displayName": "Bengaluru, India"
+  personal: {
+    name: "Sudeep G",
+    title: "Lead Software Engineer",
+    location: {
+      city: "Bengaluru",
+      state: "Karnataka",
+      country: "India",
+      displayName: "Bengaluru, India",
     },
-    "description": "Sudeep G | Lead Software Engineer | Bangalore, India",
-    "summaryParts": {
-      "prefix": "A",
-      "highlightText": "generalist",
-      "middleText": "engineer with a passion to solve problems, currently focused on",
-      "betweenText": "for the",
-      "suffix": "year of his career in Software development."
+    description: "Sudeep G | Lead Software Engineer | Bangalore, India",
+    summaryParts: {
+      prefix: "A",
+      highlightText: "generalist",
+      middleText:
+        "engineer with a passion to solve problems, currently focused on",
+      betweenText: "for the",
+      suffix: "year of his career in Software development.",
     },
-    "experience": {
-      "years": 10,
-      "focus": ["Web Development", "Machine learning", "Product building"]
-    }
+    experience: {
+      years: 10,
+      focus: ["Web Development", "Machine learning", "Product building"],
+    },
   },
-  "contact": {
-    "email": "sudeepg95@gmail.com",
-    "whatsapp": {
-      "number": "+919020709002",
-      "url": "https://wa.me/+919020709002"
+  contact: {
+    email: "sudeepg95@gmail.com",
+    whatsapp: {
+      number: "+919020709002",
+      url: "https://wa.me/+919020709002",
     },
-    "social": {
-      "github": {
-        "username": "sudeepg95",
-        "url": "https://github.com/sudeepg95"
+    social: {
+      github: {
+        username: "sudeepg95",
+        url: "https://github.com/sudeepg95",
       },
-      "linkedin": {
-        "username": "sudeepg95",
-        "url": "https://linkedin.com/in/sudeepg95"
-      }
-    }
+      linkedin: {
+        username: "sudeepg95",
+        url: "https://linkedin.com/in/sudeepg95",
+      },
+    },
   },
-  "skills": {
-    "frameworks": [
+  skills: {
+    frameworks: [
       {
-        "title": "Next.js",
-        "icon": "simple-icons:nextdotjs",
-        "url": "https://nextjs.org/"
+        title: "Next.js",
+        icon: "simple-icons:nextdotjs",
+        url: "https://nextjs.org/",
       },
       {
-        "title": "React",
-        "icon": "simple-icons:react",
-        "url": "https://reactjs.org/"
+        title: "React",
+        icon: "simple-icons:react",
+        url: "https://reactjs.org/",
       },
       {
-        "title": "Angular",
-        "icon": "simple-icons:angular",
-        "url": "https://angular.io/"
+        title: "Angular",
+        icon: "simple-icons:angular",
+        url: "https://angular.io/",
       },
       {
-        "title": "Vue",
-        "icon": "simple-icons:vuedotjs",
-        "url": "https://vuejs.org/"
+        title: "Vue",
+        icon: "simple-icons:vuedotjs",
+        url: "https://vuejs.org/",
       },
       {
-        "title": "Redux",
-        "icon": "simple-icons:redux",
-        "url": "https://redux.js.org/"
+        title: "Redux",
+        icon: "simple-icons:redux",
+        url: "https://redux.js.org/",
       },
       {
-        "title": "Express",
-        "icon": "simple-icons:express",
-        "url": "https://expressjs.com/"
-      }
+        title: "Express",
+        icon: "simple-icons:express",
+        url: "https://expressjs.com/",
+      },
     ],
-    "programming": [
+    programming: [
       {
-        "title": "JavaScript",
-        "icon": "simple-icons:javascript",
-        "url": "https://developer.mozilla.org/en-US/docs/Web/javascript"
+        title: "JavaScript",
+        icon: "simple-icons:javascript",
+        url: "https://developer.mozilla.org/en-US/docs/Web/javascript",
       },
       {
-        "title": "TypeScript",
-        "icon": "simple-icons:typescript",
-        "url": "https://www.typescriptlang.org/"
+        title: "TypeScript",
+        icon: "simple-icons:typescript",
+        url: "https://www.typescriptlang.org/",
       },
       {
-        "title": "Python",
-        "icon": "simple-icons:python",
-        "url": "https://www.python.org/"
+        title: "Python",
+        icon: "simple-icons:python",
+        url: "https://www.python.org/",
       },
       {
-        "title": "Go",
-        "icon": "simple-icons:go",
-        "url": "https://go.dev/"
+        title: "Go",
+        icon: "simple-icons:go",
+        url: "https://go.dev/",
       },
       {
-        "title": "Java",
-        "icon": "simple-icons:openjdk",
-        "url": "https://www.java.com/"
+        title: "Java",
+        icon: "simple-icons:openjdk",
+        url: "https://www.java.com/",
       },
       {
-        "title": "Bash",
-        "icon": "simple-icons:gnubash",
-        "url": "https://linuxhint.com/bash-programming-guide/"
-      }
+        title: "Bash",
+        icon: "simple-icons:gnubash",
+        url: "https://linuxhint.com/bash-programming-guide/",
+      },
     ],
-    "tools": [
+    tools: [
       {
-        "title": "HTML5",
-        "icon": "simple-icons:html5",
-        "url": "https://developer.mozilla.org/en-US/docs/Glossary/HTML5"
+        title: "HTML5",
+        icon: "simple-icons:html5",
+        url: "https://developer.mozilla.org/en-US/docs/Glossary/HTML5",
       },
       {
-        "title": "CSS3",
-        "icon": "simple-icons:css3",
-        "url": "https://developer.mozilla.org/en-US/docs/Web/CSS"
+        title: "CSS3",
+        icon: "simple-icons:css3",
+        url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
       },
       {
-        "title": "Jest",
-        "icon": "simple-icons:jest",
-        "url": "https://jestjs.io/"
+        title: "Jest",
+        icon: "simple-icons:jest",
+        url: "https://jestjs.io/",
       },
       {
-        "title": "GitHub",
-        "icon": "simple-icons:github",
-        "url": "https://github.com/"
+        title: "GitHub",
+        icon: "simple-icons:github",
+        url: "https://github.com/",
       },
       {
-        "title": "Cursor",
-        "icon": "cursor",
-        "url": "https://cursor.com/"
+        title: "Cursor",
+        icon: "cursor",
+        url: "https://cursor.com/",
       },
       {
-        "title": "VSCode",
-        "icon": "simple-icons:visualstudiocode",
-        "url": "https://code.visualstudio.com/"
-      }
-    ]
+        title: "VSCode",
+        icon: "simple-icons:visualstudiocode",
+        url: "https://code.visualstudio.com/",
+      },
+    ],
   },
-  "navigation": {
-    "resume": {
-      "url": "https://drive.google.com/file/d/1ez2cOn9VhzDl45XweK6E0kcsXiX-7aSQ/view?usp=sharing",
-      "title": "Resume"
+  navigation: {
+    resume: {
+      url: "https://sudeepg95.github.io/resume/",
+      title: "Resume",
     },
-    "sections": [
+    sections: [
       {
-        "title": "Summary",
-        "url": "#summary"
+        title: "Summary",
+        url: "#summary",
       },
       {
-        "title": "Skills",
-        "url": "#skills"
-      }
-    ]
-  }
+        title: "Skills",
+        url: "#skills",
+      },
+    ],
+  },
 };
 
 export default userProfile;
